767013666564baece6437a68e813560e24787422
Adrian Iftode <adrian.iftode@gmail.com>
2020-11-08 00:00:11 +0100

    Add registration names from https://github.com/Xabaril/AspNetCore.Diagnostics.HealthChecks

---
+++ src/AspNetCore.Diagnostics.HealthChecks.Extensions/Check.cs
@@ -0,0 +1,59 @@
+﻿// ReSharper disable All
+namespace AspNetCore.Diagnostics.HealthChecks.Extensions
+{
+    public static class Check
+    {
+        public static readonly string ArangoDB = "arangodb";
+        public static readonly string AzureBlobStorage = "azureblob";
+        public static readonly string AzureCosmosDb = "cosmosdb";
+        public static readonly string AzureEventHub = "azureeventhub";
+        public static readonly string AzureIoTHub = "iothub";
+        public static readonly string AzureKeyVault = "azurekeyvault";
+        public static readonly string AzureServiceBusQueue = "azurequeue";
+        public static readonly string AzureServiceBusTopic = "azuretopic";
+        public static readonly string AzureServiceBusSubscription = "azuresubscription";
+        public static readonly string AzureTable = "azuretable";
+        public static readonly string AzureQueueStorage = "azurequeue";
+        public static readonly string CloudFirestore = "cloud firestore";
+        public static readonly string Consul = "consul";
+        public static readonly string DiskStorage = "diskstorage";
+        public static readonly string DnsResolve = "dns";
+        public static readonly string DocumentDB = "documentdb";
+        public static readonly string DynamoDB = "dynamodb";
+        public static readonly string ElasticSearch = "elasticsearch";
+        public static readonly string Eventstore = "eventstore";
+        public static readonly string FTP = "ftp";
+        public static readonly string Gremlin = "gremlin";
+        public static readonly string Hangfire = "hangfire";
+        public static readonly string IbmMQ = "ibmmq";
+        public static readonly string IdentityServer = "idsvr";
+        public static readonly string Imap = "imap";
+        public static readonly string Kafka = "kafka";
+        public static readonly string Kubernetes = "k8s";
+        public static readonly string MongoDb = "mongodb";
+        public static readonly string MySql = "mysql";
+        public static readonly string NpgSql = "npgsql";
+        public static readonly string Oracle = "oracle";
+        public static readonly string Ping = "ping";
+        public static readonly string PrivateMemory = "privatememory";
+        public static readonly string ProcessAllocatedMemory = "process_allocated_memory";
+        public static readonly string ProcessHealth = "process";
+        public static readonly string RabbitMQ = "rabbitmq";
+        public static readonly string RavenDB = "ravendb";
+        public static readonly string Redis = "redis";
+        public static readonly string S3 = "aws s3";
+        public static readonly string SendGrid = "sendgrid";
+        public static readonly string Seq = "seq";
+        public static readonly string Sftp = "sftp";
+        public static readonly string SignalR = "signalr";
+        public static readonly string Smtp = "smtp";
+        public static readonly string Solr = "solr";
+        public static readonly string Sqlite = "sqlite";
+        public static readonly string SqlServer = "sqlserver";
+        public static readonly string Tcp = "tcp";
+        public static readonly string UrlGroup = "uri-group";
+        public static readonly string VirtualMemorySize = "virtualmemory";
+        public static readonly string WindowsService = "windowsservice";
+        public static readonly string WorkingSet = "workingset";
+    }
+}
